0a6246dc4d89a551a36666c058e041c642529afe
Matt Whiteley <matt@whiteleydesigns.com>
2021-05-14 12:52:20 -0400

    Add social media & copyright options fields

--- themes/genesis-base-child-theme/partials/footer.php
+++ themes/genesis-base-child-theme/partials/footer.php
@@ -8,6 +8,24 @@
 **/
 ?>
 
-<div class="site-footer__copyright">
-     <p>&copy; <?php echo date('Y'); ?> All Rights Reserved | Website by <a href="https://whiteleydesigns.com" target="_blank">Whiteley Designs</a></p>
-</div>
+<?php
+$wd_options_copyright_text = get_field( '$wd_options_copyright_text', 'option' );
+?>
+
+<?php if ( ! empty( $wd_options_copyright_text ) ) : ?>
+     <div class="site-footer__copyright">
+          <p>&copy; <?php echo date('Y'); ?> <?php echo $wd_options_copyright_text; ?></p>
+     </div>
+<?php endif; ?>
+
+<?php if ( have_rows( 'wd_options_social_media_links', 'option' ) ) : ?>
+     <div class="social-media">
+     	<?php while ( have_rows( 'wd_options_social_media_links', 'option' ) ) : the_row(); ?>
+
+               <a class="social-media__link" href="<?php the_sub_field( 'wd_link' ); ?>" target="_blank" rel="noopener noreferrer" aria-label="<?php the_sub_field( 'wd_aria_label' ); ?>">
+                    <?php the_sub_field( 'wd_icon' ); ?>
+               </a>
+
+     	<?php endwhile; ?>
+     </div>
+<?php endif; ?>
