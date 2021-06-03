db22d77eefc1b0bda04ab27a931c7cb1cd3448ad
Matt Whiteley <matt@whiteleydesigns.com>
2021-06-03 09:37:52 -0400

    Cleanup global script enqueues

--- themes/genesis-base-child-theme/functions.php
+++ themes/genesis-base-child-theme/functions.php
@@ -51,7 +51,7 @@ function wd_global_enqueues() {
      wp_enqueue_style(
           'wd-fonts',
           wd_theme_fonts(),
-          array(),
+          [],
           null, // remove version parameter to allow multiple fonts in string
           // 'print' // optionally defer font loading to print (performance)
      );
@@ -62,19 +62,21 @@ function wd_global_enqueues() {
           get_stylesheet_directory_uri() . '/assets/js/main-js-min.js'
      );
 
-     // font awesome
-     // wp_enqueue_script(
-     //      'wd-fontawesome',
-     //      'https://kit.fontawesome.com/7bb1dd36e7.js'
-     // );
-
      // enqueue custom stylesheet
      wp_enqueue_style(
           'wd-style',
           get_stylesheet_directory_uri() . '/assets/css/main.css',
-          array(),
+          [],
           filemtime( get_stylesheet_directory() . '/assets/css/main.css' ) // append timestamp for cache busting
      );
+
+     // font awesome
+     // wp_enqueue_script(
+     //      'wd-fontawesome',
+     //      'https://kit.fontawesome.com/7bb1dd36e7.js',
+     //      [],
+     //      null
+     // );
 
 }
 add_action( 'wp_enqueue_scripts', 'wd_global_enqueues' );
@@ -85,32 +87,38 @@ add_action( 'wp_enqueue_scripts', 'wd_global_enqueues' );
 */
 function wd_admin_enqueues() {
 
-     // custom fonts
+     // fonts
      wp_enqueue_style(
           'wd-fonts',
-          wd_theme_fonts()
-     );
-
-     // flickity
-     wp_enqueue_script(
-          'wd-flickity-admin',
-          get_stylesheet_directory_uri() . '/assets/js/src/flickity.pkgd.min.js'
-     );
-
-     // font awesome
-     wp_enqueue_script(
-          'wd-fontawesome',
-          'https://kit.fontawesome.com/7bb1dd36e7.js'
+          wd_theme_fonts(),
+          [],
+          null
      );
 
      // custom block styles
      wp_enqueue_script(
           'wd-editor',
           get_stylesheet_directory_uri() . '/assets/js/editor-min.js',
-          array( 'wp-blocks', 'wp-dom' ),
+          [ 'wp-blocks', 'wp-dom' ],
           filemtime( get_stylesheet_directory() . '/assets/js/editor-min.js' ),
           true
      );
+
+     // flickity
+     // wp_enqueue_script(
+     //      'wd-flickity-admin',
+     //      get_stylesheet_directory_uri() . '/assets/js/src/flickity.pkgd.min.js',
+     //      [],
+     //      null
+     // );
+
+     // font awesome
+     // wp_enqueue_script(
+     //      'wd-fontawesome',
+     //      'https://kit.fontawesome.com/7bb1dd36e7.js',
+     //      [],
+     //      null
+     // );
 }
 add_action( 'enqueue_block_editor_assets', 'wd_admin_enqueues' );
 
@@ -121,7 +129,7 @@ function wd_admin_style() {
      wp_enqueue_style(
           'admin-styles',
           get_stylesheet_directory_uri().'/assets/css/admin.css',
-          array(),
+          [],
           filemtime( get_stylesheet_directory() . '/assets/css/admin.css' ) // append timestamp for cache busting
      );
 }
