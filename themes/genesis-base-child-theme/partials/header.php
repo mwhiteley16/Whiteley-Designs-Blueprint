220b831ef463ed1b12edcd775dda2c6c93a042ef
Matt Whiteley <matt@whiteleydesigns.com>
2021-06-07 16:48:10 -0400

    Modify Custom Logo Output
    Allows for explicit height and width attributes on SVG logos

--- themes/genesis-base-child-theme/partials/header.php
+++ themes/genesis-base-child-theme/partials/header.php
@@ -8,12 +8,17 @@
 **/
 ?>
 
+<?php // get custom logo
+$custom_logo_id = get_theme_mod( 'custom_logo' );
+$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+?>
+
 <?php if ( function_exists( 'the_custom_logo' ) ) : ?>
-
      <div class="site-header__logo">
-          <?php the_custom_logo(); ?>
+          <a class="custom-logo-link" href="<?php echo get_home_url(); ?>">
+               <img class="custom-logo" src="<?php echo esc_url( $logo[0] ); ?>" alt="" height="" width="">
+          </a>
      </div>
-
 <?php endif; ?>
 
 <div class="mobile-navigation-icon">
