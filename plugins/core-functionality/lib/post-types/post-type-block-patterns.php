33f5fdc5276b3165c3ee43629764dafc070adbe5
Matt Whiteley <matt@whiteleydesigns.com>
2022-01-07 10:58:47 -0500

    Fix constant in plugin block pattern label

--- plugins/core-functionality/lib/post-types/post-type-block-patterns.php
+++ plugins/core-functionality/lib/post-types/post-type-block-patterns.php
@@ -74,7 +74,7 @@ function wd_register_block_pattern_categories() {
           register_block_pattern_category(
                'whiteley-designs',
                [
-                    'label' => _x( 'Whiteley Designs', 'Block pattern category', WD_CHILD_THEME_NAME ),
+                    'label' => _x( 'Whiteley Designs', 'Block pattern category', WD_PLUGIN_THEME_NAME ),
                ]
           );
 
